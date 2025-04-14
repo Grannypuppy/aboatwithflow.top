a1bea1c9fd3e580b778f040fc6261b216ca2f17e
Grannypuppy <aboatwithflow@gmail.com>
2025-04-14 16:47:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -54,7 +54,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/Grannypuppy',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.aboatwithflow.top/about',
+  HEO_INFO_CARD_URL3: 'https://www.aboatwithflow.top/waydownwego',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
